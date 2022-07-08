7510c60e5750ca41a874d2d313e617b1cdbc0b25
DiegoZacarias <diegozaca97@gmail.com>
2022-07-08 13:07:50 -0400

    passport installed + userSeeder

---
+++ tests/Feature/CardTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CardTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_can_create_a_card()
+    {
+
+    }
+}
